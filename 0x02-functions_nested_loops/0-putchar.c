baa3feceb94adba91376da94c2d4b925f29c10cc
lueenqama <queengodoz@gmail.com>
2023-02-27 08:36:24 +0100

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: print _putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+	return (0);
+}
