f1343804acc20fecf0d262e9ed0b8ee904a9db61
lueenqama <queengodoz@gmail.com>
2023-02-27 17:19:10 +0100

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of rvrn fibonacci numbers
+ * Description: computes and prints even number < 4000000
+ *  less than 4000000.
+ * Return: nothing!
+ */
+
+int main(void)
+
+{
+
+	int a =  0, b = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+
+	{
+	next = a + b;
+	a = b;
+	b = next;
+	if (next % 2 == 0)
+	sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
