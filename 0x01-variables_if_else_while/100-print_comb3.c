ecc22ffd3f7749357417a3c3ea7c37aa2d6a069c
lueenqama <queengodoz@gmail.com>
2023-05-26 21:46:46 +0100

    inventing is a combination of brains and material

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,26 +9,21 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 8; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
+			putchar(a + '0');
+			putchar(b + '0');
 
-			if (a != b)
+			if (a != 8 || b != 9)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-
-				if (a != 9 || b != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
-
 		}
-
 	}
 	putchar('\n');
+
 	return (0);
 }
