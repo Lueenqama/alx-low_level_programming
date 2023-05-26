cf7c63890c5e1514484a09557f718874e79e445c
lueenqama <queengodoz@gmail.com>
2023-05-26 20:05:01 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print alphabets except q and e'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int l = 97;
+
+	for (l = 0; l <= 9; l++)
+	{
+		printf("%d", l);
+	}
+	printf("\n");
+	return (0);
+}
