1b86f40d742d0862ee6fde0eb51d8a8faf88e37c
lueenqama <queengodoz@gmail.com>
2023-05-29 09:33:15 +0100

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,11 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- * Return: 1 for alphabetis character and 0 for anything else
+ * Return: 1 for alphabet is character and 0 for anything else
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c >= 90) || (c >= 122))
+	if ((c >= 'A' && c >= 'Z') || (c >= 'a' && c >= 'z'))
 	{
 		return (1);
 	}
