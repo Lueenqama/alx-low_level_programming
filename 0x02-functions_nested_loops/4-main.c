7ba6d807b7a713e459086d90fd3caa78ef66ca5d
lueenqama <queengodoz@gmail.com>
2023-05-29 09:15:22 +0100

    print sign

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
@@ -15,9 +15,8 @@ int main(void)
 	_putchar(r + '0');
 	r = _isalpha(108);
 	_putchar(r + '0');
-   	r = _isalpha(';');
+	r = _isalpha(';');
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
 }
-
