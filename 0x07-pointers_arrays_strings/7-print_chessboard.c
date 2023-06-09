0d28184edc53579a6d0b5ec1ba3b554547f4ab70
lueenqama <queengodoz@gmail.com>
2023-06-09 23:15:42 +0100

    print chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,21 +2,19 @@
 
 /**
  * print_chessboard - function that prints chessboard
- *
+ * @a: pointer to an array of 8 characters
  * Return: 0
  */
 
 void print_chessboard(char (*a)[8]);
 {
-	void print_chessboard(char (*a)[8])
-	{
 	int row, col;
 
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
 		{
-			putchar(a[row][col]);
+			putchar(a[row * 8 + col]);
 		}
 		putchar('\n');
 	}
