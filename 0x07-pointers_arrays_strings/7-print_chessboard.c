36c859101efda4f907f5db7dd2a5a8d5c4cd5bf5
lueenqama <queengodoz@gmail.com>
2023-06-10 00:08:17 +0100

    print chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -19,19 +19,3 @@ void print_chessboard(char (*a)[8]);
 		_putchar('\n');
 	}
 }
-
-/**
- * main - check the code
- *
- * Return: 0
- */
-int main(void)
-{
-	char *chessboard = malloc(sizeof(char) * 8 * 8);
-
-	print_chessboard(chessboard);
-
-	free(chessboard);
-
-	return (0);
-}
