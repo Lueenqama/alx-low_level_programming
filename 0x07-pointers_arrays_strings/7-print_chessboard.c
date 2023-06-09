6c2e9521c3c247fbe64e08cdd6c9c93d913719b6
lueenqama <queengodoz@gmail.com>
2023-06-09 20:55:10 +0100

    print chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+void _putchar(char c);
 
 /**
  * print_chessboard - this function prints a chessboard
@@ -8,18 +8,46 @@
 
 void print_chessboard(char (*a)[8])
 {
-	printf("   a b c d e f g h\n");
-	printf("  -----------------\n");
+	char column[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+	char horizontal_line[] = "-----------------\n";
+	char row_label[2] = {'8', '\n'};
 
 	for (int i = 0; i < 8; i++)
 	{
-		printf("%d ", 8 - i);
+		_putchar(' ');
+		_putchar(column[i]);
+	}
+	_putchar('\n');
+
+	for (int i = 0; i < sizeof(horizontal_line) - 1; i++)
+	{
+		_putchar(horizontal_line[i]);
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		row_label[0] = '8' - i;
+		_putchar(row_label[0]);
+		_putchar(' ');
+
 		for (int j = 0; j < 8; j++)
 		{
-			printf("%c ", a[i][j]);
+			_putchar(a[i][j]);
+			_putchae(' ');
 		}
-		printf("%d\n", 8 - i);
+
+		_putchar(row_label[0]);
+		_putchar('\n');
 	}
-	printf("  -----------------\n");
-	printf("   a b c d e f g h\n");
+
+	for (int i = 0; i < sizeof(horozontal_line) - 1; i++)
+	{
+		_putchar(horizontal_line[i]);
+	}
+
+	for (int i = 0; i < 8; I++)
+	{
+		_putchar(' ');
+		_putchar(column[i]);
+	}
+	_putchat('\n');
 }
