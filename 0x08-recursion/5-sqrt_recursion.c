a54503a7d0ef074f0556fafc38d7711ea323a0f3
lueenqama <queengodoz@gmail.com>
2023-06-14 16:54:45 +0100

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,38 +3,47 @@
 /**
  * _sqrt_recursion - the function returns
  * the natural square root of a number
+ *
+ * sqrt_helper: this is a helper function
  * @start: begining of range
  * @end: equal to n
  * @n: parameter
+ *
+ * main - demonstrates usage of _sqrt_recursion function
  * Return: result
  */
+int sqrt_helper(int n, int start, int end)
+{
+	int mid;
+	int square;
+
+	if (start > end)
+	{
+		return (end);
+	}
+
+	mid = start + (end - start) / 2;
+	square = mid * mid;
+
+	if (square == n)
+	{
+		return (mid);
+	}
+
+	if (square > n)
+	{
+		return (sqrt_helper(n, start, mid - 1));
+	}
+
+	return (sqrt_helper(n, mid + 1, end));
+}
+
 int _sqrt_recursion(int n)
 {
-	int result;
-	int start = 1;
-	int end = n;
-
 	if (n == 0 || n == 1)
 	{
 		return (n);
 	}
 
-	while (start <= end)
-	{
-		int mid = (start + end) / 2;
-
-		if (mid * mid == n)
-		{
-			return (mid);
-		}
-
-		if (mid * mid < n)
-		{
-			start = mid + 1;
-			result = mid;
-		} else
-			end = mid - 1;
-	}
-
-	return (result);
+	return (sqrt_helper(n, 1, n));
 }
